23310117149963565f0fe145226d1bf980c65829
ajayarankeri <ajayarankeri@gmail.com>
2019-07-31 12:12:42 +0530

    file upload change committed

--- src/main/java/com/hcl/productdb/controller/ProductController.java
+++ src/main/java/com/hcl/productdb/controller/ProductController.java
@@ -1,12 +1,19 @@
 package com.hcl.productdb.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("")
 public class ProductController {
 	
-	
+	@PostMapping("/uploadFile")
+    public void uploadFile(@RequestParam("file") MultipartFile file) {
+		
+		
+	}
 
 }
